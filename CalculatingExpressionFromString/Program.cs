3b4fd2adff8161e52655d155f2ea46abd221fe5e
goatl <goatlegflex@gmail.com>
2022-10-05 18:36:51 +0300

    Добавьте файлы проекта.

---
+++ CalculatingExpressionFromString/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Linq;
+using System.Text.RegularExpressions;
+using ExpressionAnalayzer;
+
+Console.WriteLine("Введите формулу:");
+string? str = Console.ReadLine();
+
+if(str != null && ExpressionAnalayzerClass.IsMeaningfulExpression(str))
+{
+    Console.WriteLine("Enter var value");
+    double x = Convert.ToDouble(Console.ReadLine());
+    string[] strArr = ExpressionAnalayzerClass.ConvertToStringArray(str);
+    Console.WriteLine($"result ={ExpressionAnalayzerClass.GetAnswer(strArr[2], ExpressionAnalayzerClass.getFunc(strArr[1], x), ExpressionAnalayzerClass.getFunc(strArr[3], x))}");
+    
+}
+else
+{
+    Console.WriteLine("Введено неверное выражение");
+}
+Console.ReadLine();
---
+++ ExpressionAnalayzerClass/ExpressionAnalayzer.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ExpressionAnalayzer
+{
+    public class ExpressionAnalayzerClass
+    {
+        static Regex fillRx = new Regex(@"(cos|sin|tg|ctg)");
+        static private readonly Regex rx = new Regex(@"[a-z]\([a-z]\)\=(sin|cos|tg|ctg)\([a-z]\)[\+\-\*](sin|cos|tg|ctg)\([a-z]\)");
+        static Regex replaceRx = new Regex(@"\([a-z]\)");
+
+        public static bool IsMeaningfulExpression(string? str)
+        {
+            return rx.IsMatch(str);
+        }
+
+        public static string[] ConvertToStringArray(string str)
+        {
+            str = replaceRx.Replace(str, "");
+            return fillRx.Split(str);
+        }
+        public static double GetAnswer(string mathOperator, double x, double y)
+        {
+            switch (mathOperator)
+            {
+                case "-":
+                    return x - y;
+                case "+":
+                    return x + y;
+                case "*":
+                    return x * y;
+                case null:
+                    return 0;
+            }
+            return 0;
+        }
+        public static double getFunc(string strFunc, double x)
+        {
+
+            switch (strFunc)
+            {
+                case "cos":
+                    return Math.Cos(x);
+                case "sin":
+                    return Math.Sin(x);
+                case "tg":
+                    return Math.Tan(x);
+                case "ctg":
+                    return 1 / Math.Tan(x);
+            }
+            return 0;
+        }
+    }
+}
